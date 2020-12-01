ff4ba483909ae531d6427007e463d28edb68d7dc
Brennan Colberg <brennan.colberg@gmail.com>
2020-12-01 07:53:29 -0800

    add tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/dist/next-server/lib/router/router"
-import "../styles/globals.css"
+import "../styles/index.css"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// const colors = require("tailwindcss/colors")
+
+module.exports = {
+  purge: ["./pages/**/*", "./components/**/*"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require("@tailwindcss/typography")
+  ],
+}
